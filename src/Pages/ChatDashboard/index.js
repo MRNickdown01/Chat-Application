33149ee77d5f7522b38b3bde478a2c0f967aec11
MRNickdown01 <dubeynick123@gmail.com>
2024-06-16 21:28:20 +0530

    functionalty done

--- src/Pages/ChatDashboard/index.js
+++ src/Pages/ChatDashboard/index.js
@@ -1,11 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Modal from "../../Component/Modal";
-import {
-  Description,
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-} from "@headlessui/react";
+
 import {
   Popover,
   PopoverButton,
@@ -17,6 +11,7 @@ const ChatDashboard = () => {
   const [selectChat, setSelectChat] = useState([]);
   const [open, setOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+  const [arrowBack, setArrowBack] = useState(false);
 
   const data = [
     {
@@ -158,155 +153,198 @@ const ChatDashboard = () => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
     };
+    if (window?.innerWidth < 768) {
+      setArrowBack(true);
+    }
     window.addEventListener("resize", handleResize);
+    const userChat = document?.getElementById("userChat");
+    userChat.style.display = "none";
     handleResize();
     return () => {
       window.addEventListener("resize", handleResize);
     };
   }, []);
   console.log(selectChat);
+  console.log(isMobile);
+  /** arrow back function **/
+
+  const arrowBackClick = () => {
+    if (window?.innerWidth < 768) {
+      const chatList = document?.getElementById("chatList");
+      chatList.style.display = "block";
+      const userChat = document?.getElementById("userChat");
+      userChat.style.display = "none";
+    }
+  };
+
+  /** handle click for show all chats for one specific users **/
+
+  const handleClick = (i) => {
+    setSelectChat(i);
+    if (window?.innerWidth < 768) {
+      const chatList = document?.getElementById("chatList");
+      chatList.style.display = "none";
+      const userChat = document?.getElementById("userChat");
+      userChat.style.display = "block";
+    } else {
+      const chatList = document?.getElementById("chatList");
+      chatList.style.display = "block";
+      const userChat = document?.getElementById("userChat");
+      userChat.style.display = "block";
+    }
+  };
+
+  console.log(isMobile, "isMobile");
 
   return (
     <div>
       <div className="flex h-screen overflow-hidden">
-        {!isMobile && (
-          <div className="w-1/4 bg-white border-r border-gray-300">
-            <header className="p-4 border-b border-gray-300 flex justify-between items-center  text-black">
-              <h1 className="text-2xl font-semibold">Chats</h1>
-            </header>
+        {/***
+         *  chat list section
+         *
+         */}
+        <div
+          className="lg:w-1/4 w-full bg-white border-r border-gray-300"
+          id="chatList"
+        >
+          <header className="p-4 border-b border-gray-300 flex justify-between items-center  text-black">
+            <h1 className="text-2xl font-semibold">Chats</h1>
+          </header>
+          <div className="overflow-y-auto h-screen p-3 mb-9 pb-20">
+            {data?.map((i, index) => {
+              return (
+                <div
+                  className="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md"
+                  key={index}
+                  onClick={() => handleClick(i)}
+                >
+                  <div className="w-12 h-12 bg-gray-300 rounded-full mr-3">
+                    <img
+                      src={i?.profilePictureURL}
+                      alt="User Avatar"
+                      className="w-12 h-12 rounded-full"
+                    />
+                  </div>
+                  <div className="flex-1">
+                    <h2 className="text-lg font-semibold text-start">
+                      {i?.name}
+                    </h2>
+                    <p className="text-gray-600 text-start">
+                      {i?.chat[0]?.user1?.message}
+                    </p>
+                  </div>
 
-            <div className="overflow-y-auto h-screen p-3 mb-9 pb-20">
-              {data?.map((i, index) => {
-                return (
-                  <div
-                    className="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md"
-                    key={index}
-                    onClick={() => setSelectChat(i)}
-                  >
-                    <div className="w-12 h-12 bg-gray-300 rounded-full mr-3">
+                  <Popover>
+                    <PopoverButton className="text-sm/6 font-semibold text-black/50 focus:outline-none data-[active]:text-black data-[hover]:text-white data-[focus]:outline-1 data-[focus]:outline-white">
                       <img
-                        src={i?.profilePictureURL}
-                        alt="User Avatar"
-                        className="w-12 h-12 rounded-full"
-                      />
-                    </div>
-                    <div className="flex-1">
-                      <h2 className="text-lg font-semibold text-start">
-                        {i?.name}
-                      </h2>
-                      <p className="text-gray-600 text-start">
-                        {i?.chat[0]?.user1?.message}
-                      </p>
-                    </div>
-
-                    <Popover>
-                      <PopoverButton className="text-sm/6 font-semibold text-black/50 focus:outline-none data-[active]:text-black data-[hover]:text-white data-[focus]:outline-1 data-[focus]:outline-white">
-                        <img
-                          className="w-4"
-                          src="assets/images/ellipsis.png"
-                          onClick={() => setOpen(true)}
-                        ></img>
-                      </PopoverButton>
-                      <Transition
-                        enter="transition ease-out duration-200"
-                        enterFrom="opacity-0 translate-y-1"
-                        enterTo="opacity-100 translate-y-0"
-                        leave="transition ease-in duration-150"
-                        leaveFrom="opacity-100 translate-y-0"
-                        leaveTo="opacity-0 translate-y-1"
+                        className="w-4"
+                        src="assets/images/ellipsis.png"
+                        alt="3-dots"
+                        onClick={() => setOpen(true)}
+                      ></img>
+                    </PopoverButton>
+                    <Transition
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 translate-y-1"
+                    >
+                      <PopoverPanel
+                        anchor="bottom"
+                        className="divide-y divide-white/5 rounded-xl bg-white/5 text-sm/6 [--anchor-gap:var(--spacing-5)]"
                       >
-                        <PopoverPanel
-                          anchor="bottom"
-                          className="divide-y divide-white/5 rounded-xl bg-white/5 text-sm/6 [--anchor-gap:var(--spacing-5)]"
-                        >
-                          <div className="p-3 bg-[#fafafa]">
-                            <div className="block rounded-lg py-2 px-3 transition cursor-pointer hover:bg-gray-100">
-                              <p className="font-semibold text-black">
-                                Mark as Unread
-                              </p>
-                            </div>
-                            <a
-                              className="block rounded-lg py-2 px-3 transition hover:bg-white/5"
-                              href="#"
-                            >
-                              <p className="font-semibold text-black">Delete</p>
-                            </a>
-                            <a
-                              className="block rounded-lg py-2 px-3 transition hover:bg-white/5"
-                              href="#"
-                            >
-                              <p className="font-semibold text-black">Cancel</p>
-                            </a>
+                        <div className="p-3 bg-[#fafafa]">
+                          <div className="block rounded-lg py-2 px-3 transition cursor-pointer hover:bg-gray-100">
+                            <p className="font-semibold text-black">
+                              Mark as Unread
+                            </p>
                           </div>
-                        </PopoverPanel>
-                      </Transition>
-                    </Popover>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        )}
-        {selectChat ? (
-          <div className="flex-1">
-            <header className="bg-[#fafafa] p-2 text-gray-700">
-              <div className="flex">
-                <div className="flex items-center gap-4 w-12 h-12 bg-gray-300 rounded-full mr-3 ">
-                  <img
-                    src={selectChat?.profilePictureURL}
-                    alt="User Avatar"
-                    className="w-12 h-12 rounded-full object-cover"
-                  />
+                          <div className="block rounded-lg py-2 px-3 transition cursor-pointer hover:bg-gray-100">
+                            <p className="font-semibold text-black">Delete</p>
+                          </div>
+                          <div className="block rounded-lg py-2 px-3 transition cursor-pointer hover:bg-gray-100">
+                            <p className="font-semibold text-black">Cancel</p>
+                          </div>
+                        </div>
+                      </PopoverPanel>
+                    </Transition>
+                  </Popover>
                 </div>
-                <h1 className="text-2xl font-semibold text-start">
-                  {selectChat?.name}
-                </h1>
+              );
+            })}
+          </div>
+        </div>
+
+        {/***
+         * user all chats section
+         *
+         */}
+        <div className="flex-1" id="userChat">
+          <header className="bg-[#fafafa] p-2 text-gray-700">
+            <div className="flex">
+              {arrowBack && (
+                <img
+                  src="assets/images/back-arrow.png"
+                  className="w-10"
+                  alt="arrow"
+                  onClick={() => arrowBackClick()}
+                />
+              )}
+              <div className="flex items-center gap-4 w-12 h-12 bg-gray-300 rounded-full mr-3 ">
+                <img
+                  src={selectChat?.profilePictureURL}
+                  alt="User Avatar"
+                  className="w-12 h-12 rounded-full object-cover"
+                />
               </div>
-              <div></div>
-            </header>
-            <div className="h-screen overflow-y-auto p-4 pb-36">
-              {selectChat?.chat?.map((chats, index) => {
-                return (
-                  <div key={index}>
-                    <div className="flex mb-4 cursor-pointer">
-                      <div className="max-w-9/12 bg-[#fafafa] rounded-2xl p-2 gap-3">
-                        <p className="text-gray-700">
-                          {chats?.[selectChat?.userId]?.message}
-                        </p>
-                        <div className="text-end text-xs">
-                          {chats?.[selectChat?.userId]?.timeStamp}
-                        </div>
-                      </div>
-                    </div>
-                    <div className="flex justify-end mb-4 cursor-pointer">
-                      <div className="max-w-9/12 bg-[#dcf6c6] text-black rounded-2xl p-3 gap-3 text-start">
-                        <p>{chats?.you?.message}</p>
-                        <div className="text-end text-xs">
-                          {chats?.you?.timeStamp}
-                        </div>
+              <h1 className="text-2xl font-semibold text-start">
+                {selectChat?.name}
+              </h1>
+            </div>
+            <div></div>
+          </header>
+          <div className="h-screen overflow-y-auto p-4 pb-36">
+            {selectChat?.chat?.map((chats, index) => {
+              return (
+                <div key={index}>
+                  <div className="flex mb-4 cursor-pointer">
+                    <div className="max-w-9/12 bg-[#fafafa] rounded-2xl p-2 gap-3">
+                      <p className="text-gray-700">
+                        {chats?.[selectChat?.userId]?.message}
+                      </p>
+                      <div className="text-end text-xs">
+                        {chats?.[selectChat?.userId]?.timeStamp}
                       </div>
                     </div>
                   </div>
-                );
-              })}
-            </div>
-
-            <footer className="bg-white border-t border-gray-300 p-4 absolute bottom-0 lg:w-3/4 w-full">
-              <div className="flex items-center">
-                <input
-                  type="text"
-                  placeholder="Type a message..."
-                  className="w-full p-2 rounded-md border border-gray-400 focus:outline-none focus:border-blue-500"
-                />
-                <button className="bg-indigo-500 text-white px-4 py-2 rounded-md ml-2">
-                  Send
-                </button>
-              </div>
-            </footer>
+                  <div className="flex justify-end mb-4 cursor-pointer">
+                    <div className="max-w-9/12 bg-[#dcf6c6] text-black rounded-2xl p-3 gap-3 text-start">
+                      <p>{chats?.you?.message}</p>
+                      <div className="text-end text-xs">
+                        {chats?.you?.timeStamp}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
-        ) : (
-          ""
-        )}
+
+          <footer className="bg-white border-t border-gray-300 p-4 absolute bottom-0 lg:w-3/4 w-full">
+            <div className="flex items-center">
+              <input
+                type="text"
+                placeholder="Type a message..."
+                className="w-full p-2 rounded-md border border-gray-400 focus:outline-none focus:border-blue-500"
+              />
+              <button className="bg-indigo-500 text-white px-4 py-2 rounded-md ml-2">
+                Send
+              </button>
+            </div>
+          </footer>
+        </div>
       </div>
     </div>
   );
